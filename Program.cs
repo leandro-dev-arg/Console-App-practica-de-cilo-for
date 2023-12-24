b10be27f3ca23352ef402cc325fc883eecec20ea
leandro <leandro.dev.arg@gmail.com>
2023-12-23 21:01:35 -0300

    subida inicial unidad 5-ejercicio 3

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace U5_programa_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   int contador,acumulador,edad,promedio;
+            acumulador=0;
+            contador=0;
+            for (int x = 0; x < 20; x++)
+            {
+                Console.WriteLine("ingrese una edad por favor");
+                edad=int.Parse(Console.ReadLine());
+                if (edad>=18){
+                    contador++;
+                    acumulador+=edad;
+                }
+            }
+            promedio=acumulador/contador;
+            Console.WriteLine("El promedio de edades de personas masyores a 18  es "+ promedio+ " años");
+        }
+    }
+}
